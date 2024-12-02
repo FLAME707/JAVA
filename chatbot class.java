568cf9759a06fe36ef8c0a1baaf1a6d047932dff
FLAME707 <122411974+FLAME707@users.noreply.github.com>
2024-12-02 09:32:57 -0800

    chatbot class.java

---
+++ chatbot class.java
@@ -0,0 +1,8 @@
+public class chat {
+private String name;
+public chat(String nname) {
+name = nname; }
+public void cw(String txt) {
+System.out.print("\nbot--{      " + txt + "\n\n" + name + "--{      ");
+}
+}
